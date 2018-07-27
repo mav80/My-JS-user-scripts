0fb4eef84f31ab0b6a9a2cf38869446b59b4bed9
“mav80” <mav80@prokonto.pl>
2018-07-27 10:12:22 +0200

    PCLab.pl-UnfoldNews.user.js - major changes to script for more robust handling of links and non-standard news (that doesn't have certain parts expected).

--- PCLab.pl-UnfoldNews.user.js
+++ PCLab.pl-UnfoldNews.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         PCLab.pl-UnfoldNews.user.js
-// @version      1.0
-// @description  This script loads contents of all the news on the "aktualnoœci" (news) page - no need to click "wiêcej" anymore.
-// @author       Witcher
+// @version      1.1
+// @description  This script loads contents of all the news on the "aktualnosci" (news) page - no need to click "wiecej" (more) anymore.
+// @author       Mav
 // @updateURL    https://mav.matcom.com.pl/MyJSUserScripts/PCLab.pl-UnfoldNews.user.js
 // @downloadURL  https://mav.matcom.com.pl/MyJSUserScripts/PCLab.pl-UnfoldNews.user.js
 // @include      https://pclab.pl/news.html
@@ -13,6 +13,8 @@
 // @include      https://pclab.pl/news-*-50.html
 // @require      https://code.jquery.com/jquery-latest.min.js
 // @grant        none
+//last update:   2018-08-24
+
 // ==/UserScript==
 
 (function() {
@@ -20,13 +22,50 @@
 
     var elements = $('.element');
 
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].children[2].firstElementChild.remove(); //removes doubled head image
-        elements[i].children[2].lastElementChild.style.textAlign="justify";
-        $(elements[i].children[2].lastElementChild).load(elements[i].children[4].firstElementChild.href + ' #main > .content.news > .main > div.substance > div.data');
-        elements[i].children[4].remove(); //deletes unnecessary 'wiêcej' button
+    if(elements.length > 0) {
+        for(var i = 0; i < elements.length; i++) {
+
+            //if news has a head image, we remove because it will be load again - prevents element doubling
+            var elementImgToRemove = $(elements[i]).find('img');
+            if(elementImgToRemove != null) {
+                $(elementImgToRemove[0]).remove();
+            }
+
+
+
+            //searches for a text element and attempts to set text align to "justify"
+            //$(elements[i]).find('.text').find('p')[0].style.textAlign = "justify";
+            var elementTextToJustify = $(elements[i]).find('.text').find('p');
+            if(elementTextToJustify.length > 0) {
+                elementTextToJustify[0].style.textAlign = "justify";
+            }
+
+
+
+            //main part - looks for a "wiêcej (more)" element, extracts link and loads it
+            //$($(elements[i]).find('.text').find('p')[0]).load($(elements[i]).find('.more')[0].firstElementChild.href + ' #main > .content.news > .main > div.substance > div.data');
+            var textElementToExtend = $(elements[i]).find('.text').find('p')[0];
+            if(textElementToExtend != null) {
+                var elementWithLink =  $($(elements[i]).find('.more')[0]).find('a')[0];
+                if(elementWithLink != null) {
+                    var linkToLoad = $($(elements[i]).find('.more')[0]).find('a')[0].href;
+                    if(linkToLoad != null) {
+                        $(textElementToExtend).load(linkToLoad + ' #main > .content.news > .main > div.substance > div.data');
+                    }
+                }
+            }
+
+
+
+            //deletes unnecessary 'wiecej' button
+            var elementMoreToRemove = $(elements[i]).find('.more');
+            if(elementMoreToRemove.length > 0) {
+                elementMoreToRemove[0].remove();
+            }
+
+        }
+    } else {
+        console.log('PCLab - unfold news - no news found, aborting.');
     }
-
-
 
 })();
